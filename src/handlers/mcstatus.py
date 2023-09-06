4a9c2962d129ef09141eaa086cee449e73c7ca48
Karel <noppes3000@gmail.com>
2023-09-06 12:50:58 +0200

    ignore api changes, they crash too often

--- src/handlers/mcstatus.py
+++ src/handlers/mcstatus.py
@@ -8,8 +8,7 @@ class McStatusCog(commands.Cog):
     def __init__(self, bot, logger):
         self.logger = logger
         self.channel = bot.get_channel(references.message_mcstatus_channel_id)
-        self.prev_status = self.status()
-        self.check_status = self.status()
+        self.prev_status = False
         self.count = 0
         self.status_check.start()
 
@@ -33,6 +32,7 @@ class McStatusCog(commands.Cog):
                 url = "https://minecraftstatus.net/"
             )
             formatted_keys = ["**" + key +":**" for key in result.keys()]
+            
             embed.add_field(name="", value='\n'.join(formatted_keys), inline=True)
             embed.add_field(name="", value='\n'.join(result.values()), inline=True)
             await ctx.send(embed=embed)
@@ -40,7 +40,15 @@ class McStatusCog(commands.Cog):
     @tasks.loop(seconds=60)
     async def status_check(self):
         result = self.status()
-        if result and self.prev_status != result:
+        if not result:
+            return
+        api_value = result.pop('api')
+
+        if not self.prev_status:
+            self.prev_status = result
+            return
+
+        if self.prev_status != result:
             self.count += 1
             if(self.count < 6):#making sure the status is changed for multiple minutes
                 return
@@ -53,7 +61,8 @@ class McStatusCog(commands.Cog):
                 description=description
             )
             formatted_keys = ["**" + key +":**" for key in result.keys()]
+            formatted_keys.append("**api:**")
             embed.add_field(name="", value='\n'.join(formatted_keys), inline=True)
-            embed.add_field(name="", value='\n'.join(result.values()), inline=True)
+            embed.add_field(name="", value='\n'.join(result.values()) + '\n' + api_value, inline=True)
             await self.channel.send(embed=embed) 
 
